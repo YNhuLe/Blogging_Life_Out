91b9f77c69356417d22886b97e78f151e4d0ed53
NhuYLe <lenhuy10011996@gmail.com>
2025-06-08 23:46:39 -0400

    switch to PostgreSql

--- static/js/post.js
+++ static/js/post.js
@@ -1,15 +1,13 @@
-//clear out the comment text field after submission
-//wait for the DOM fully loaded
+
 document.addEventListener("DOMContentLoaded", function(){
     document.getElementById("myForm").addEventListener('submit', function(event){
 
         setTimeout(() => {
         const editor = CKEDITOR.instances['comment_text'];
-        if (editor){ // check if the editor instance exist
-            editor.setData('');  //clears the content of the CKEditor field after the form submission is completed
-
+        if (editor){
+            editor.setData('');
         }
-    }, 100);// add the delay which allows the submission to complete
+    }, 100);
     });
 
 });
